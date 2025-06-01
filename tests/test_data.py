6dc326c6c0a7d050b861e644ad6047b5dbb32886
bhroben <robenbhatti@gmail.com>
2025-06-01 17:14:56 +0200

    test connection

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,14 @@
 import pytest
+import psycopg2
 
 
-def test_example():
-    assert 1 + 1 == 2
+def test_connection():
 
+    try:
+        conn = psycopg2.connect(
+            dbname="datawarehouse", user="postgres", password="postgres", host="localhost", port="5432"
+        )
+        conn.close()
+        assert True
+    except Exception as e:
+        assert False, f"Connection failed: {e}"
